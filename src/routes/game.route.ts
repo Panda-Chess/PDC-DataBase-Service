72ed298e9250ab7daebfae45aa5e9676aa97922c
Bogdan <bogdanrazvan00@yahoo.com>
2024-05-07 14:35:34 +0300

    fix: 🐛 modifyGame route will return 200 status with modified

--- src/routes/game.route.ts
+++ src/routes/game.route.ts
@@ -51,7 +51,7 @@ router.get("/unstarted-games/:gameType", async (req, res) => {
 router.put("/:id", async (req, res) => {
     const game: Game = await modifyGame(req.params.id, req.body);
 
-    res.status(204).json(game);
+    res.status(200).json(game);
 });
 
 router.post("/", async (req, res) => {
